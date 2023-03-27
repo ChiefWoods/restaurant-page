96476f1a0c4b3fd1435edb36748b7c2e2b534a7a
Chii Yuen <chii.yuen@hotmail.com>
2023-03-27 23:59:31 +0800

    Set up dependencies

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('testing 9');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHlCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCd0ZXN0aW5nIDknKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
