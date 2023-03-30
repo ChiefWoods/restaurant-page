e22a81a79fabe0d19b51e860b423c3810170fa38
Chii Yuen <chii.yuen@hotmail.com>
2023-03-31 02:57:00 +0800

    Adjust menu texts

--- src/intro.js
+++ src/intro.js
@@ -8,12 +8,12 @@ export function createIntro() {
   section.classList.add('container-intro');
 
   const blockquote = document.createElement('blockquote');
-  blockquote.textContent = "QUACK QUACK QUACK QUACK QUACK QUACK QUACK";
+  blockquote.textContent = '"QUACK QUACK QUACK QUACK QUACK QUACK QUACK"';
 
   const figcaption = document.createElement('figcaption');
   figcaption.textContent = "- Wolfgang Duck";
 
-  const span = createSpan("Neighbourhood friendly BBQ place, smoking since 2012. We put so much love into our food, that we only open three days a week. Our meats are so popular, we literally don't even have a closing time. They sell out before you're even done reading this.");
+  const span = createSpan("Neighbourhood friendly BBQ place, smoking since 2012. We put so much love into our food, that we'd only open three days a week. Our meats are so popular, we literally don't even have a closing time. That's how legendary they are.");
 
   section.append(blockquote, figcaption, span);
   main.appendChild(section);
--- src/menu.js
+++ src/menu.js
@@ -15,17 +15,17 @@ export function createMenu() {
   const section = document.createElement('section');
   section.classList.add('container-menu');
 
-  section.appendChild(createMenuGroup('APPETIZER', [
-    createMenuItem('BBQ Buffalo Bites', 'Baked wings tossed in buffalo sauce, served along a side of celery sticks and blue cheese dip.'),
-    createMenuItem('Carolina Hush Puppies', 'Deep friend cornbread, served with a miniature farmhouse coleslaw.')
+  section.appendChild(createMenuGroup('APPETIZERS', [
+    createMenuItem('BBQ Buffalo Bites', 'Baked wings tossed in buffalo sauce, served alongside celery sticks and blue cheese dip.'),
+    createMenuItem('Carolina Hush Puppies', 'Deep fried cornbread, served with a miniature farmhouse coleslaw.')
   ]));
 
   section.appendChild(createMenuGroup('MEATS', [
-    createMenuItem('Texas Barbecued Brisket', `What you are really here for. Smoked for 12 hours, this 1lb behemoth comes with our homemade BBQ dip for good measure.`),
-    createMenuItem('Memphis Ribs', 'Pork ribs smoked over charcoal in a vinegar rub, finished with herbal and earthy seasoning.'),
+    createMenuItem('Texas Barbecued Brisket', `What you are really here for. Smoked for 12 hours. 50% fatty, 50% lean, 100% satisfaction guaranteed.`),
+    createMenuItem('Memphis Ribs', 'Pork ribs smoked over charcoal in a vinegar rub, finished with a herbal and earthy seasoning.'),
     createMenuItem("Kansas City Sweet n' Smoky Ribs", 'Baby backs rubbed in a supreme blend of spices, smoked and basted in barbecue sauce.'),
     createMenuItem('Honey Marinated Rib Eye Steak', 'Grain-fed and grilled over the flame. Served with gravied mash potatoes and farmhouse coleslaw.'),
-    createMenuItem('Carolina Pulled Pork', 'Slow cooked juicy pork shoulder, shredded cabbage, layered between sesame buns.')
+    createMenuItem('Carolina Pulled Pork', 'Slow-cooked pork shoulder, shredded cabbage, layered between two sesame buns with a healthy smothering of aioli.')
   ]))
 
   section.appendChild(createMenuGroup('SIDES', [
@@ -36,13 +36,13 @@ export function createMenu() {
 
   section.appendChild(createMenuGroup('DESSERTS', [
     createMenuItem('Apple Pie', 'Granny smiths encased in a golden brown crust. Served with vanilla ice cream.'),
-    createMenuItem('Chocolate Chip Cookies', 'We’ll let the cookies themselves do the talking.'),
+    createMenuItem('Chocolate Chip Cookies', "We'll let the cookies themselves do the talking."),
     createMenuItem('Strawberry Cheese Cake', 'Lucious cream cheese filling baked on a graham cracker crust, topped off with strawberry puree. Served with a dollop of whipped cream.')
   ]))
 
   section.appendChild(createMenuGroup('DRINKS', [
     createMenuItem('Peach Iced Tea', 'Black tea infused with peaches, served refreshingly.'),
-    createMenuItem('Lemonade', 'Life gave us lemons, lots of lemons.')
+    createMenuItem('Lemonade', 'Life gave us lemons, but we want to serve you sweetness instead.')
   ]))
 
   main.appendChild(section);
