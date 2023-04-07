ad1954532a8b76c46a2999471179fcdd258b180f
Chii Yuen <chii.yuen@hotmail.com>
2023-04-07 14:35:52 +0800

    Add template.html and plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      favicon: './src/icons/bbq_sauce.png',
+      template: './src/template.html'
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    // clean: true
   },
   module: {
     rules: [
